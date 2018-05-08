34a351bd5bcd982fe80c8c918037166e789a89ab
Alex Tang <alextang1030@gmail.com>
2018-05-08 13:27:10 +0000

    fix confirm error

--- app/models/user_history.rb
+++ app/models/user_history.rb
@@ -1,7 +1,7 @@
 class UserHistory < ApplicationRecord
 
 	belongs_to :user, foreign_key: "user_id"
-	belongs_to :event, foreign_key: "id"
+	belongs_to :event, foreign_key: "event_id"
 	belongs_to :restaurant, foreign_key: "restaurant_id"
 	
 end
